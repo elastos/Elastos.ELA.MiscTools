74d8cb0d5d883dcf71fd12932c14389e438e8368
tandonglei <tandonglei28@gmail.com>
2021-06-08 21:43:01 +0800

    Add DID v0.3.1 upgrade script

---
+++ docs/did/elastos_supernode_update_elastos-did-v0.3.1.md
@@ -0,0 +1,33 @@
+# did节点升级方法[20210608]
+
+did节点 elastos-did-v0.3.1 版本已发布，该版本升级内容主要包括：
+- 添加did凭证验证
+- 增加推送CustomID相关交易的布隆过滤器
+
+## 升级步骤
+
+1. 登录服务器
+2. 进入节点目录
+
+```bash
+cd ~/node/
+```
+
+3. 下载节点更新脚本
+
+```bash
+wget https://raw.githubusercontent.com/elastos/Elastos.ELA.MiscTools/master/script/did/update_elastos-did-v0.3.1.sh;
+chmod a+x ~/node/update_elastos-did-v0.3.1.sh
+```
+
+4. 执行节点更新脚本
+
+```bash
+~/node/update_elastos-did-v0.3.1.sh
+```
+
+5. 检查节点运行状态
+
+```bash
+~/node/node.sh status
+```
---
+++ script/did/update_elastos-did-v0.3.1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+# record update log
+#
+update_log()
+{
+  if [ ! -f $SCRIPT_PATH/update.log ]; then
+    echo "$SCRIPT_PATH/update.log is not exist"
+    echo "Create update.log"
+    touch $SCRIPT_PATH/update.log
+  fi
+
+  local time=$(date "+%Y-%m-%d %H:%M:%S")
+  echo $time>>$SCRIPT_PATH/update.log
+  echo "==========">>$SCRIPT_PATH/update.log
+  echo "Update did to update_elastos-did-v0.3.1">>$SCRIPT_PATH/update.log
+  echo "">>$SCRIPT_PATH/update.log
+  echo "$time Update did succeeded!"
+  echo "Please check update log via command: cat ~/node/update.log"
+}
+
+#
+# update node binary
+#
+update_node()
+{
+  if [ ! -f $SCRIPT_PATH/node.sh ]; then
+    echo "ERROR: $SCRIPT_PATH/node.sh is not exist"
+    exis
+  fi
+
+  wget https://download.elastos.org/elastos-did/elastos-did-v0.3.1/elastos-did-v0.3.1-linux-x86_64.tgz
+  wget https://download.elastos.org/elastos-did/elastos-did-v0.3.1/SHA256SUMS
+  shasum -c SHA256SUMS
+  tar xf elastos-did-v0.3.1-linux-x86_64.tgz
+  ~/node/node.sh did stop
+  cp  elastos-did-v0.3.1/did ~/node/did/
+  rm -r ~/node/did/elastos_did/data/spv/
+  ~/node/node.sh did start
+  rm -r elastos-did-v0.3.1-linux-x86_64.tgz elastos-did-v0.3.1 SHA256SUMS
+}
+
+SCRIPT_PATH=$(cd $(dirname $BASH_SOURCE); pwd)
+update_node
+update_log
