83000db487fbfcfe6b241fe59f473bac2c5cc406
tandonglei <tandonglei28@gmail.com>
2021-11-13 14:35:59 +0800

    add eid-oracle v0.1.3.2 upgrade script

---
+++ docs/eid-oracle/elastos-eid-oracle-v0.1.3.2.md
@@ -0,0 +1,49 @@
+# The EID-Oracle v0.1.3.2 upgrade[20211113]
+
+The upgrade mainly includes:
+- Support new recharge transactions of ELA nodes 
+
+## Automatic upgrade steps
+
+1. Log in to the server
+2. Enter the node directory
+
+```bash
+cd ~/node/
+```
+
+3. Download node update script
+
+```bash
+wget https://raw.githubusercontent.com/elastos/Elastos.ELA.MiscTools/master/upgrade/eid-oracle/elastos-eid-oracle-v0.1.3.2.sh
+```
+
+4. Script permission changes
+
+```bash
+chmod a+x ~/node/elastos-eid-oracle-v0.1.3.2.sh
+```
+
+5. Execute node update script
+
+```bash
+~/node/elastos-eid-oracle-v0.1.3.2.sh
+```
+
+6. Check node operating status
+
+```bash
+~/node/node.sh status
+```
+
+## Manual upgrade steps
+
+1. Download eid-oracle
+
+```bash
+https://download.elastos.org/elastos-eid-oracle/elastos-eid-oracle-v0.1.3.2
+```
+
+2. Close eid-oracle
+3. Replace the js file of eid-oracle
+4. Start eid-oracle
---
+++ upgrade/eid-oracle/elastos-eid-oracle-v0.1.3.2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo_error()
+{
+    echo -e "\033[1;31mERROR:\033[0m $1"
+}
+
+echo_info()
+{
+    echo -e "\033[1;34mINFO:\033[0m $1"
+}
+
+#
+# record update log
+#
+update_log()
+{
+  if [ ! -f $SCRIPT_PATH/update.log ]; then
+    echo_error "$SCRIPT_PATH/update.log is not exist"
+    echo_info "Create update.log"
+    touch $SCRIPT_PATH/update.log
+  fi
+
+  local time=$(date "+%Y-%m-%d %H:%M:%S")
+  echo_info $time>>$SCRIPT_PATH/update.log
+  echo_info "==========">>$SCRIPT_PATH/update.log
+  echo_info "Update eid-oracle to elastos-eid-oracle-v0.1.3.2">>$SCRIPT_PATH/update.log
+  echo_info "">>$SCRIPT_PATH/update.log
+  echo_info "$time Update eid-oracle succeeded!"
+  echo_info "Please check update log via command: cat $SCRIPT_PATH/update.log"
+}
+
+#
+# update node binary
+#
+upgrade_node()
+{
+  if [ ! -f $SCRIPT_PATH/node.sh ]; then
+    echo_error "$SCRIPT_PATH/node.sh is not exist"
+    exit
+  fi
+
+  if [ ! -d $SCRIPT_PATH/eid/eid-oracle ]; then
+    echo_error "$SCRIPT_PATH/eid/eid-oracle is not exist"
+    exit
+  fi
+
+  echo_info "Downloading eid-oracle..."
+  wget https://download.elastos.org/elastos-eid-oracle/elastos-eid-oracle-v0.1.3.2/elastos-eid-oracle-v0.1.3.2.tgz
+  wget https://download.elastos.org/elastos-eid-oracle/elastos-eid-oracle-v0.1.3.2/SHA256SUMS
+  shasum -c SHA256SUMS
+  tar xf elastos-eid-oracle-v0.1.3.2.tgz
+  echo_info "Stopping eid-oracle..."
+  $SCRIPT_PATH/node.sh eid-oracle stop
+  echo_info "Replacing eid-oracle..."
+  cp -v elastos-eid-oracle-v0.1.3.2/* $SCRIPT_PATH/eid/eid-oracle/
+  echo_info "Starting eid-oracle..."
+  $SCRIPT_PATH/node.sh eid-oracle start
+  rm -r elastos-eid-oracle-v0.1.3.2.tgz elastos-eid-oracle-v0.1.3.2.sh elastos-eid-oracle-v0.1.3.2 SHA256SUMS
+}
+
+SCRIPT_PATH=$(cd $(dirname $BASH_SOURCE); pwd)
+upgrade_node
+update_log
