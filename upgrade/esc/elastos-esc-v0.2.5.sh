16605801effebccca6c0108a45bf7960a2b56de6
y1234ljbmfh <y1234linjianbin@126.com>
2024-09-12 15:56:28 +0800

    Update elastos-esc-v0.2.5.sh

--- upgrade/esc/elastos-esc-v0.2.5.sh
+++ upgrade/esc/elastos-esc-v0.2.5.sh
@@ -24,7 +24,7 @@ update_log()
   local time=$(date "+%Y-%m-%d %H:%M:%S")
   echo_info $time>>$SCRIPT_PATH/update.log
   echo_info "==========">>$SCRIPT_PATH/update.log
-  echo_info "Update esc to elastos-esc-v0.2.1">>$SCRIPT_PATH/update.log
+  echo_info "Update esc to elastos-esc-v0.2.5">>$SCRIPT_PATH/update.log
   echo_info "">>$SCRIPT_PATH/update.log
   echo_info "$time Update esc succeeded!"
   echo_info "Please check update log via command: cat $SCRIPT_PATH/update.log"
@@ -41,18 +41,18 @@ upgrade_node()
   fi
 
   echo_info "Downloading esc..."
-  wget https://download.elastos.io/elastos-esc/elastos-esc-v0.2.1/elastos-esc-v0.2.1-linux-x86_64.tgz
-  wget https://download.elastos.io/elastos-esc/elastos-esc-v0.2.1/SHA256SUMS
+  wget https://download.elastos.io/elastos-esc/elastos-esc-v0.2.5/elastos-esc-v0.2.5-linux-x86_64.tgz
+  wget https://download.elastos.io/elastos-esc/elastos-esc-v0.2.5/SHA256SUMS
   shasum -c SHA256SUMS
-  tar xf elastos-esc-v0.2.1-linux-x86_64.tgz
+  tar xf elastos-esc-v0.2.5-linux-x86_64.tgz
   echo_info "Stopping esc..."
   $SCRIPT_PATH/node.sh esc stop
   echo_info "Replacing esc..."
-  cp -v elastos-esc-v0.2.1-linux-x86_64/esc $SCRIPT_PATH/esc/
+  cp -v elastos-esc-v0.2.5-linux-x86_64/esc $SCRIPT_PATH/esc/
 
   echo_info "Starting esc..."
   $SCRIPT_PATH/node.sh esc start
-  rm -r elastos-esc-v0.2.1-linux-x86_64.tgz elastos-esc-v0.2.1.sh elastos-esc-v0.2.1-linux-x86_64 SHA256SUMS
+  rm -r elastos-esc-v0.2.5-linux-x86_64.tgz elastos-esc-v0.2.5.sh elastos-esc-v0.2.5-linux-x86_64 SHA256SUMS
 }
 
 SCRIPT_PATH=$(cd $(dirname $BASH_SOURCE); pwd)
